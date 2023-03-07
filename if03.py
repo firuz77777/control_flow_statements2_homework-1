aec3693443de51f951b765733e4558d37df0f3a8
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2023-03-07 17:16:12 +0500

    3

--- if03.py
+++ if03.py
@@ -7,5 +7,23 @@ def main(a,b,c):
         c: Third number.
     Returns:
         int: return answer.
-    """
-    return
+    """ 
+    aa = 0
+    mx = a
+    if mx < b:
+        mx = b
+    if mx < c:
+        mx = c
+    mm = a
+    if mm > b:
+        mm = b
+    if mm > c:
+        mm = c
+    if mx > a > mm:
+        aa = a
+    if mx > b > mm:
+        aa = b
+    if mx > c > mm:
+        aa = c
+    return aa
+print(main(3,9,4))
